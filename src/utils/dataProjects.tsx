97b7557e403fb2b4406f6d32204c5161b6ff17e0
RomainRG31 <romain32.roger@gmail.com>
2024-05-25 08:19:26 +0200

    add new project: HappyBeersDay

--- src/utils/dataProjects.tsx
+++ src/utils/dataProjects.tsx
@@ -6,23 +6,28 @@ interface Project {
 
 export const dataProjects: Project[] = [
   {
-    order: "01 ° 04",
+    order: "01 ° 05",
     title: "title1",
     link: "https://github.com/RomainRG31/romainspipboy",
   },
   {
-    order: "02 ° 04",
+    order: "02 ° 05",
     title: "title2",
     link: "https://moovieslib.netlify.app",
   },
   {
-    order: "03 ° 04",
+    order: "03 ° 05",
+    title: "title3",
+    link: "https://happybeersday.vercel.app",
+  },
+  {
+    order: "04 ° 05",
     title: "title3",
     link: "https://bmi-calculate-chi.vercel.app/",
   },
 
   {
-    order: "04 ° 04",
+    order: "05 ° 05",
     title: "title4",
     link: "https://kasa-nu-seven.vercel.app/Home",
   },
@@ -34,20 +39,24 @@ interface Translations {
     title2: string;
     title3: string;
     title4: string;
+    title5: string;
   };
 }
 
 export const projectTranslations: Translations = {
   fr: {
-    title1: "Pipboy de Romain - Traiteur d'informations personnelles",
+    title1: "Pipboy de Romain - Traiteur d'informations personnelles vintage",
     title2: "MooviesLib - Vidéothèque en ligne",
-    title3: "Calculateur d'IMC",
-    title4: "Kasa - Site de location immobilière",
+    title3: "HappyBeersDay - Guide intéractif de choix de bière selon l'humeur",
+    title4: "Calculateur d'IMC",
+    title5: "Kasa - Site de location immobilière",
   },
   en: {
-    title1: "Pipboy by Romain - Processor of personal information",
+    title1: "Pipboy by Romain - Vintage Processor of personal information",
     title2: "MooviesLib - Online video library",
-    title3: "BMI calculator",
-    title4: "Kasa - Real Estate Rental Website",
+    title3:
+      "Happy BirthDay - Interactive guide to beer choices depending on your mood",
+    title4: "BMI calculator",
+    title5: "Kasa - Real Estate Rental Website",
   },
 };
