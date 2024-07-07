a41337b83d191de3ba92d7557fe79bfa20d0d930
RomainRG31 <romain32.roger@gmail.com>
2024-07-07 17:20:23 +0200

    feat: add easyaccess

--- src/utils/dataProjects.tsx
+++ src/utils/dataProjects.tsx
@@ -6,29 +6,34 @@ interface Project {
 
 export const dataProjects: Project[] = [
   {
-    order: "01 ° 05",
+    order: "01 ° 06",
     title: "title1",
+    link: "https://easy-access-wcs.vercel.app",
+  },
+  {
+    order: "02 ° 06",
+    title: "title2",
     link: "https://github.com/RomainRG31/romainspipboy",
   },
   {
-    order: "02 ° 05",
-    title: "title2",
+    order: "03 ° 06",
+    title: "title3",
     link: "https://moovieslib.netlify.app",
   },
   {
-    order: "03 ° 05",
-    title: "title3",
+    order: "04 ° 06",
+    title: "title4",
     link: "https://happybeersday.vercel.app",
   },
   {
-    order: "04 ° 05",
-    title: "title4",
+    order: "05 ° 06",
+    title: "title5",
     link: "https://bmi-calculate-chi.vercel.app/",
   },
 
   {
-    order: "05 ° 05",
-    title: "title5",
+    order: "06 ° 06",
+    title: "title6",
     link: "https://kasa-nu-seven.vercel.app/Home",
   },
 ];
@@ -40,23 +45,26 @@ interface Translations {
     title3: string;
     title4: string;
     title5: string;
+    title6: string;
   };
 }
 
 export const projectTranslations: Translations = {
   fr: {
-    title1: "Pipboy de Romain - Traiteur d'informations personnelles vintage",
-    title2: "MooviesLib - Vidéothèque en ligne",
-    title3: "HappyBeersDay - Guide intéractif de choix de bière selon l'humeur",
-    title4: "Calculateur d'IMC",
-    title5: "Kasa - Site de location immobilière",
+    title1: "EasyAccess - Jeu de sensibilisation à la mobilité réduite",
+    title2: "Pipboy de Romain - Traiteur d'informations personnelles vintage",
+    title3: "MooviesLib - Vidéothèque en ligne",
+    title4: "HappyBeersDay - Guide intéractif de choix de bière selon l'humeur",
+    title5: "Calculateur d'IMC",
+    title6: "Kasa - Site de location immobilière",
   },
   en: {
-    title1: "Pipboy by Romain - Vintage Processor of personal information",
-    title2: "MooviesLib - Online video library",
-    title3:
+    title1: "EasyAccess - Reduced mobility awareness game",
+    title2: "Pipboy by Romain - Vintage Processor of personal information",
+    title3: "MooviesLib - Online video library",
+    title4:
       "HappyBeersDay - Interactive guide to beer choices depending on your mood",
-    title4: "BMI calculator",
-    title5: "Kasa - Real Estate Rental Website",
+    title5: "BMI calculator",
+    title6: "Kasa - Real Estate Rental Website",
   },
 };
